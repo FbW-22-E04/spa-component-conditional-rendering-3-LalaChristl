591d2ebe164a2abcad4e0ab856aa138a0e622529
LalaChristl <lesleyannchristl@gmail.com>
2022-10-11 15:35:19 +0200

    added soltion

--- src/Alert.js
+++ src/Alert.js
@@ -1,12 +1,17 @@
 import React from "react"
 const Alert = props => {
 	//add the condition inside this function
-
+if(props.show === false) {
+	return null;
+}
+else {
 	return (
 		<div className="alert alert-danger" role="alert">
-			This is a primary alert-check it out!
+			{props.text}
 		</div>
-	);
+	);	
+}
+
 };
 
 export default Alert;
